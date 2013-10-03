336524316e9f0ad18bb22c9fec3f4dc04086840c
inframatic <isteffy@hotmail.com>
2013-10-03 17:01:23 -0400

    Topic link no longer vanishes when clicked as the web page going to the specific post. This is accomplished by nesting the route 'posts' in 'post'.

--- js/app.js
+++ js/app.js
@@ -2,8 +2,10 @@ App = Ember.Application.create();
 
 App.Router.map(function () {
 	this.resource('about');
-	this.resource('posts');
-	this.resource('post', { path: ':post_id'});
+	this.resource('posts', function() {
+		this.resource('post', { path: ':post_id'});
+	});
+	
 });
 
 App.PostsRoute = Ember.Route.extend ({
@@ -19,4 +21,11 @@ var posts = [{
 	date: new Date('10-03-2013'),
 	excerpt: "I was born a Charmander",
 	body: "Did you hear theyre making a Mega-Charizard? Wtf."
-}]
+	},  {
+	id: '2',
+	title: "A tail of on Rail",
+	author: { name: 'Blastoise'},
+	date: new Date('11-22-2012'),
+	excerpt: "The cannons on my back",
+	body: "The installation of the cannons on my back was a 2 month surgery"
+	}];
