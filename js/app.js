c32475751aa70e6dd5aa028e16bedaaa22f3c0d5
inframatic <isteffy@hotmail.com>
2013-10-03 12:46:21 -0400

    Added some dummy data in app.js for the variable post

--- js/app.js
+++ js/app.js
@@ -5,4 +5,11 @@ App.Router.map(function () {
 	this.resource('forums');
 });
 
-var posts = []
+var posts = [{
+	id:'1',
+	title: "All Hail Rails",
+	author: { name: 'Charizard'},
+	date: new Date('10-03-2013'),
+	excerpt: "I was born a Charmander",
+	body: "Did you hear theyre making a Mega-Charizard? Wtf."
+}]
