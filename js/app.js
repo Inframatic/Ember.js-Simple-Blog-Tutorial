f6c2ec047c233088a53c51e49114bcbe3eb81ac1
inframatic <isteffy@hotmail.com>
2013-10-06 16:52:36 -0400

    Finished Bloggr Emember tutorial

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@ var posts = [{
 	author: { name: 'Charizard'},
 	date: new Date('10-03-2013'),
 	excerpt: "I was born a Charmander",
-	body: "Did you hear theyre making a Mega-Charizard? Wtf."
+	body: "Did you hear theyre making a Mega-Charizard?"
 	},  {
 	id: '2',
 	title: "A tail of on Rail",
@@ -29,3 +29,32 @@ var posts = [{
 	excerpt: "The cannons on my back",
 	body: "The installation of the cannons on my back was a 2 month surgery"
 	}];
+  
+App.PostRoute = Ember.Route.extend({
+  model: function(params) {
+    return posts.findBy('id', params.post_id);
+  }
+});
+
+App.PostController = Ember.ObjectController.extend({
+  isEditing: false,
+
+  edit: function() {
+    this.set('isEditing', true);
+  },
+
+  doneEditing: function() {
+    this.set('isEditing', false);
+    this.get('store').commit();
+  }
+});
+
+var showdown = new Showdown.converter();
+
+Ember.Handlebars.helper('format-markdown', function(input) {
+  return new Handlebars.SafeString(showdown.makeHtml(input));
+});
+
+Ember.Handlebars.helper('format-date', function(date) {
+  return moment(date).fromNow();
+});
